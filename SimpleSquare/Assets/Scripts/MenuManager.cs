b484e9a801070702d10e51199cd6fee752f2ec03
Kisame712 <kisamehoshigakiuhmw712@gmail.com>
2025-08-23 14:23:31 +0530

    Added basic main menu and player movement

---
+++ SimpleSquare/Assets/Scripts/MenuManager.cs
@@ -0,0 +1,24 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+#if UNITY_EDITOR
+using UnityEditor;
+#endif
+public class MenuManager : MonoBehaviour
+{
+ 
+    public void PlayGame()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+    public void ExitGame()
+    {
+#if UNITY_EDITOR
+        EditorApplication.ExitPlaymode();
+#else
+        Application.Quit();
+#endif
+    }
+
+    
+}
